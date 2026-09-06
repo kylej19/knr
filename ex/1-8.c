b73b5dbd2a7636e7d8261fd52126f9331377efe6
kylej <kmj19@tuta.io>
2026-09-05 20:50:55 -0500

    init

---
+++ ex/1-8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int b, c, t, nl;
+    b = 0;
+    t = 0;
+    nl = 0;
+    while ((c = getchar()) != EOF) {
+        if (c = ' ')
+            b++;
+        if (c = '\t')
+            t++;
+        if (c = '\n')
+            nl++;
+    }
+    printf("Blanks: %d\nTabs: %d\nNewlines: %d\n", b, t, nl);
+}
---
+++ ex/htoi.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int htoi(char s[])
+{
+    int c, n;
+    for(int i = 0; (c = getchar()) != '\0' && c != '\n'; i++) 
+    {
+
